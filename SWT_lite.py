35d1d2bb342520786eed6eee0fb39e92b6424c94
kirill klimov <ra22341@ya.ru>
2012-10-28 19:39:56 +0400

    add anglediff

--- SWT_lite.py
+++ SWT_lite.py
@@ -30,12 +30,19 @@ def gradient(image, anchor):
     return np.arccos(dx / g)
 
 def dirselect(angle):
-    if angle == None: return None
+    if (angle == None) or (np.isnan(angle)): return None    
     delta = np.pi / 4
     #turns = [(1, 0), (1, 1), (0, 1), (-1, 1), (-1, 0), (-1, -1), (0, -1), (1, -1)]
     turns = [(1, 0), (1, -1), (0, -1), (-1, -1), (-1, 0), (-1, 1), (0, 1), (1, 1)]
     direct = (angle / delta)
     return turns[int(np.round(direct)) % 8]
+
+def anglediff(f, s):
+    tmpangle = min([abs(f - s),
+        abs(np.pi*2 + f - s),
+        abs(np.pi*2 - f + s)])
+    return tmpangle
+
 
 def Stroke(img, mask, point):
     makestep = lambda point, step: (point[0] + step[0], point[1] + step[1])
@@ -43,20 +50,36 @@ def Stroke(img, mask, point):
             (point[0] + 1 < image.shape[0]) and (point[1] + 1 < image.shape[1]) and
             (point [0] - 1 > 0) and (point [1] - 1 > 0))
     stroke = []
-    if not checkbound(point, image): return 
-
+    if not checkbound(point, image): return []
+    oldangle = gradient(image, point)
+    angle = oldangle
+    if (oldangle == None) or (np.isnan(oldangle)): return []
+    
 
 img = cv2.imread('img/numbers/1.jpg')
+cv2.imshow('orig', img)
 
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 gray = cv2.Canny(gray, 10, 230)
-gradient (gray, (6, 4))
-cv2.waitKey(1000)
+
+edges = []
+
 extracted = np.zeros(gray.shape)
+extracted[:] = float('nan')
+for i in xrange(1, gray.shape[0]-1):
+    for j in xrange(1, gray.shape[1]-1):
+        if gray[i, j] == 255:
+            edges.append((i, j))
+        extracted[i, j] = gradient(gray, (i, j))
 
+stepmap = [ [dirselect(-x) for x in vect] for vect in extracted]
+for p in edges:
+    print extracted[p[0], p[1]]
 
-cv2.imshow('gay', extracted)
+#print extracted
+#print stepmap
 cv2.imshow('gray', gray)
+cv2.imshow('ext', extracted)
 cv2.waitKey(1000)
 
 '''
@@ -78,7 +101,5 @@ A = np.array([
 print convolution(A, Gx)
 print convolution(A, Gy)
 print dirselect(gradient(A, (1, 1)))
-for i in xrange(1, gray.shape[0]-1):
-    for j in xrange(1, gray.shape[1]-1):
-        extracted[i, j] = gradient(gray, (i, j))
+
 '''
