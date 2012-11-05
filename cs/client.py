03b0da8e8ac9701f37d966d9e0fdf5f958001ca5
kirill klimov <ra22341@ya.ru>
2012-11-05 09:55:40 +0400

    add serv-client sample

---
+++ cs/client.py
@@ -0,0 +1,19 @@
+from socket import *
+
+HOST = 'localhost'
+PORT = 21568
+BUFSIZ = 1024
+ADDR = (HOST, PORT)
+
+tcpCliSock = socket(AF_INET, SOCK_STREAM)
+tcpCliSock.connect(ADDR)
+
+while 1:
+    data = raw_input('> ')
+    if not data: break 
+    tcpCliSock.send(data)
+    data = tcpCliSock.recv(1024)
+    if not data: break 
+    print data
+
+tcpCliSock.close()
---
+++ cs/server.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from socket import * 
+from threading import *
+def handler(clientsock,addr):
+    while 1:
+        data = clientsock.recv(BUFSIZ)
+        if not data: break 
+        clientsock.send('echoed:..' + data)
+  
+    clientsock.close()
+    
+if __name__=='__main__': 
+    HOST = 'localhost'
+    PORT = 21568
+    BUFSIZ = 1024
+    ADDR = (HOST, PORT)
+    serversock = socket(AF_INET, SOCK_STREAM)
+    serversock.bind(ADDR)
+    serversock.listen(2)
+    while 1:
+        print 'waiting for connection…'
+        clientsock, addr = serversock.accept()
+        print '…connected from:', addr
+        t = Thread(target=handler, args=(clientsock, addr))
+        t.start()
+
