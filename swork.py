575e1501b8ef10611690967565dae59bb3d57ff4
kirill klimov <ra22341@ya.ru>
2012-11-23 12:31:41 +0400

    minor changes and small test was passed

--- swork.py
+++ swork.py
@@ -57,10 +57,13 @@ if __name__ == '__main__':
             debug_flags=CUR_DEBUG_FLAGS
             #debug_flags=DEFAULT_DEBUG_FLAGS
         )
+
     print len(letters)
     for l in letters:
         for p in l['points']:
-            image[p[0], p[1]] = (205, 200, 0)
-    cv2.imwrite('res.jpg', image)
-    cv2.imshow('1111111', image)
-    cv2.waitKey(100000)
+            image[p[0], p[1]] = (205, 100, 0)
+        cv2.rectangle(image, (l['Y'], l['X']), (l['Y2'], l['X2']), (0, 200, 0))
+    #writing final result
+    cv2.imwrite('result/' + sys.argv[1].replace('/', '-'), image)
+    cv2.imshow('after all ...', image)
+    cv2.waitKey(100)
