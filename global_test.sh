3eb50147ec28a73aa34a74ceb2c76bd1ad572220
kirill klimov <ra22341@ya.ru>
2012-11-23 12:47:08 +0400

    minor changes

--- global_test.sh
+++ global_test.sh
@@ -4,6 +4,7 @@ python swork.py img/pure/3.jpg p3
 python swork.py img/numbers/1.jpg n1 
 python swork.py img/numbers/2.jpg n2 
 python swork.py img/numbers/3.jpg n3 
+python swork.py img/cars/vin.jpg v1
 python swork.py img/cars/1.jpg c1 
 python swork.py img/cars/2.jpg c2
 python swork.py img/cars/3.jpg c3
