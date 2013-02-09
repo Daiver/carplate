50abb48b8d06446fd4f454b202d5a10dcb11d7fc
Daiver <ra22341@ya.ru>
2013-02-09 17:35:10 +0400

    first work with components

--- marking.py
+++ marking.py
@@ -7,26 +7,45 @@ def show_n_write_components(img_path):
     if not os.path.exists(img_path):
         print 'Cannot find image', img_path
         return
-    
+    print('Work with %s' % img_path)
     image = cv2.imread(img_path)
     letters = FindLetters(image, 
             debug_flags=DEFAULT_DEBUG_FLAGS
         )
 
-    del letters[0]['points']
-    del letters[0]['swvalues']
-    newltr = json.dumps(letters[0])
-    print json.loads(newltr)
-    for l in letters:
-        cv2.rectangle(image, (l['Y'], l['X']), (l['Y2'], l['X2']), (0, 20, 200))
-    #writing final result
-    cv2.imwrite('result/' + img_path.replace('/', '-'), image)
-    #cv2.imshow('after all ...', image)
-    #cv2.waitKey(10000)
+    #print json.loads(newltr)
+    new_path = 'dumps/' + img_path.replace('/', '-')
+    with open(new_path, 'w') as f:
+        for l in letters:
+            del l['points']
+            del l['swvalues']
+            cv2.rectangle(image, (l['Y'], l['X']), (l['Y2'], l['X2']), (0, 20, 200))
+            l['path'] = img_path
+            cv2.imshow('after all ...', image)
+            key = cv2.waitKey() % 0x100
+            print(key)
+            newltr = json.dumps(l)
+            print(newltr)
+            f.write(newltr + '\n')
+
+def getsubs(dir):
+    dirs = []
+    files = []
+    for dirname, dirnames, filenames in os.walk(dir):
+        dirs.append(dirname)
+        for subdirname in dirnames:
+            dirs.append(os.path.join(dirname, subdirname))
+        for filename in filenames:
+            files.append(os.path.join(dirname, filename))
+    return dirs, files
 
 
 if __name__ == '__main__':
     img_path = 'img/cars/2.jpg'
+    dir_path = '/home/kirill/fromavtochmo'
+    print('Scaning dir %s....' % dir_path)
+    dirs, files = getsubs(dir_path)
+    print('Finding %s durs, %s files' % (str(len(dirs)), str(len(files))))
     show_n_write_components(img_path)
 
 #{'std': 1.3543538616436788, 'X2': 50, 'height': 62, 'width': 48, 'centerX': 26.0, 'centerY': 33.0, 'Y': 2, 'X': 2, 'Y2': 64, 'mean': 2.9627906976744187}
