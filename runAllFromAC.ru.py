93bd4dc569a28223081bc871916bcdc0a0e68555
Daiver <ra22341@ya.ru>
2013-02-15 14:57:24 +0400

    ...

--- runAllFromAC.ru.py
+++ runAllFromAC.ru.py
@@ -20,7 +20,7 @@ if __name__ == '__main__':
     dirs, files = getsubs(dir_path)
     files = filter(lambda x: x[-4:] == '.jpg', files)
     print('Finding %s durs, %s files' % (str(len(dirs)), str(len(files))))
-    border_index = 0
+    border_index = 990
     for i, i_name in enumerate(files[border_index:]):
         i += border_index
         print('Number %s' % str(i))
