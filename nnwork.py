29d54da4576d0306e92eb6b933b8d040695a5688
Daiver <ra22341@ya.ru>
2013-02-15 18:55:56 +0400

    minor changes

--- nnwork.py
+++ nnwork.py
@@ -5,6 +5,8 @@ from pybrain.supervised.trainers import BackpropTrainer, rprop
 import pickle
 import numpy as np
 from time import time
+import simplejson as json
+import os
 
 NN_in_size = 6
 
@@ -18,8 +20,17 @@ def features_from_component(component):
             float(component['mean']),
         ])
 
-def train_it():
+def ds_from_raw_data(source_dir):
+    res = []
+    for source_file in os.listdir(source_dir):
+        with open(os.path.join(source_dir, source_file)) as f:
+            for line in f:
+                res.append(json.loads(line))
+    print(len(res))
     ds = SupervisedDataSet(NN_in_size, 1)
+    return ds
+
+def train_it():
     net = buildNetwork(NN_in_size, 12, 1, bias=True)
     for i in xrange(1, 5):
         ds.addSample(np.array([1, 0.1 * i]), (1, ))
@@ -30,4 +41,6 @@ def train_it():
     trainer.trainOnDataset(ds, 10)
     trainer.testOnData()
     print 'Learning time:', time() - st
-    print (net.activate(np.array([0, 0.2])))
+
+if __name__ == '__main__':
+    ds_from_raw_data('training_examples')
