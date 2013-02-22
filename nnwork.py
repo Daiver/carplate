aedcc53f35054128603902f612e596db10e018a2
Daiver <ra22341@ya.ru>
2013-02-23 01:43:41 +0400

    ...

--- nnwork.py
+++ nnwork.py
@@ -39,10 +39,10 @@ def ds_from_raw_data(source_dir):
     return ds
 
 def train_it(ds):
-    net = buildNetwork(NN_in_size, 36, 1, bias=True)
+    net = buildNetwork(NN_in_size, 54, 1, bias=True)
     st = time()
     trainer = rprop.RPropMinusTrainer(net, learningrate = 0.01, momentum = 0.99, verbose=True)
-    trainer.trainOnDataset(ds, 100)
+    trainer.trainOnDataset(ds, 200)
     trainer.testOnData()
     print 'Learning time:', time() - st
     return net
